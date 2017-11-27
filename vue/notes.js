b66c5b8ab1453f1c4cd0950d93bf551d55f0b98b
zekebergida <zekebergida@gmail.com>
2017-11-26 20:17:24 -0500

    finish with vue

--- vue/notes.js
+++ vue/notes.js
@@ -1,17 +1,27 @@
 var app = new Vue({
   el: '#app',
   data: {
-    notes: [
+    notes: [],
+    selectedNote: {},
+    searchNoteText: ""
+  },
+  mounted: function() {
+    this.notes = [
       {id: 1, body: "This is a first test", timestamp: Date.now()},
       {id: 2, body: "This is a second test", timestamp: Date.now()},
       {id: 3, body: "This is a third test", timestamp: Date.now()}
-    ]
+    ];
+    this.selectedNote = this.notes[0];
   },
   computed: {
     transformedNotes: function() {
-      return this.notes.slice().sort(function(a, b) {
-        return b.timestamp - a.timestamp;
-      });
+      return this.notes
+        .filter(function(note) {
+          return note.body.toLowerCase().indexOf(this.searchNoteText.toLowerCase()) !== -1;
+        }.bind(this))
+        .sort(function(a, b) {
+          return b.timestamp - a.timestamp;
+        });
     }
   },
   filters: {
@@ -30,10 +40,43 @@ var app = new Vue({
     }
   },
   methods: {
-    
+    selectNote: function(note) {
+      this.selectedNote = note;
+    },
+    createNote: function() {
+      var newNote = {
+        id: Date.now(),
+        body: "",
+        timestamp: Date.now()
+      };
+      this.notes.push(newNote);
+      this.selectedNote = newNote;
+    },
+    deleteNote: function(note) {
+      var index = this.notes.indexOf(this.selectedNote);
+      if (index !== -1) {
+        this.notes.splice(index, 1);
+        if (this.transformedNotes.length > 0) {
+          this.selectedNote = this.transformedNotes[0];
+        } else {
+          this.selectedNote = {};
+        }
+      }
+    }
+  },
+  watch: {
+    notes: {
+      handler: function() {
+        this.selectedNote.timestamp = Date.now();
+      },
+      deep: true
+    },
+    searchNoteText: function() {
+      if (this.transformedNotes.length === 0) {
+        this.selectedNote = {};
+      } else if (this.transformedNotes.indexOf(this.selectedNote) === -1) {
+        this.selectedNote = this.transformedNotes[0];
+      }
+    }
   }
-
-
-
-
 });
